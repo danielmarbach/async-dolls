ad9ceba522effcecdb8cdee28600bac0193240e1
danielmarbach <daniel.marbach@openplace.net>
2015-09-11 20:58:50 +0200

    Full demo of snapshotting

--- async-dolls/3-AsyncDolls/Script.cs
+++ async-dolls/3-AsyncDolls/Script.cs
@@ -193,6 +193,69 @@ namespace AsyncDolls
             var action = actions.Dequeue();
             return action(context, () => Invoke(context, actions));
         }
+
+        [Test]
+        public async Task NowItGetsALittleBitCrazy()
+        {
+            var actions = new Queue<Func<Context, Func<Task>, Task>>();
+
+            actions.Enqueue(MethodAsyncWithContext1);
+            actions.Enqueue(MethodWhichExecutesRestMultipleTimes);
+            actions.Enqueue(MethodAsyncWithContext2);
+            actions.Enqueue(MethodAsynWithContextc3);
+
+            var context = new Context();
+            context.SetLogger(Console.WriteLine);
+            
+            var invoker = new PipelineInvoker();
+            invoker.Actions(actions);
+            context.SetSnapshotter(invoker.TakeSnapshot, invoker.RestoreSnapshot);
+
+            await invoker.Invoke(context);
+        }
+
+        static async Task MethodWhichExecutesRestMultipleTimes(Context context, Func<Task> next)
+        {
+            context.GetLogger()("MethodWhichExecutesRestMultipleTimes");
+            for (int i = 0; i < 3; i++)
+            {
+                using (context.TakeSnapshot())
+                {
+                    await next().ConfigureAwait(false);
+                }
+            }
+        }
+
+        class PipelineInvoker
+        {
+            readonly Stack<Queue<Func<Context, Func<Task>, Task>>>  stack = new Stack<Queue<Func<Context, Func<Task>, Task>>>();
+            Queue<Func<Context, Func<Task>, Task>>  executingActions = new Queue<Func<Context, Func<Task>, Task>>();
+
+            public void Actions(IEnumerable<Func<Context, Func<Task>, Task>> actions)
+            {
+                executingActions = new Queue<Func<Context, Func<Task>, Task>>(actions);
+            }
+
+            public void TakeSnapshot()
+            {
+                stack.Push(new Queue<Func<Context, Func<Task>, Task>>(executingActions));
+            }
+
+            public void RestoreSnapshot()
+            {
+                // Slightly evil, but reference assignments are atomic
+                executingActions = stack.Pop();
+            }
+
+            public Task Invoke(Context context)
+            {
+                if (executingActions.Count == 0)
+                    return Task.FromResult(0);
+
+                var action = executingActions.Dequeue();
+                return action(context, () => Invoke(context));
+            }
+        }
     }
 
     class Context
@@ -226,6 +289,42 @@ namespace AsyncDolls
         class Logger
         {
             public Action<string> Debug { get; internal set; } 
+        }
+    }
+
+    static class ContextSnapshotExtensions
+    {
+        public static void SetSnapshotter(this Context context, Action take, Action restore)
+        {
+            var snapshotter = new Snapshotter { Take = take, Restore = restore };
+            context.Set(snapshotter);
+        }
+
+        public static IDisposable TakeSnapshot(this Context context)
+        {
+            return new SnapshotRegion(context);
+        }
+
+        class Snapshotter
+        {
+            public Action Take { get; internal set; }
+            public Action Restore { get; internal set; }
+        }
+
+        class SnapshotRegion : IDisposable
+        {
+            private readonly Context context;
+
+            public SnapshotRegion(Context context)
+            {
+                this.context = context;
+                context.Get<Snapshotter>().Take();
+            }
+
+            public void Dispose()
+            {
+                context.Get<Snapshotter>().Restore();
+            }
         }
     }
 }
