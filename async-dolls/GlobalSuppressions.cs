cd1e42d172753e4689b31ba87526369c50430a83
danielmarbach <daniel.marbach@openplace.net>
2016-05-09 21:27:29 +0200

    Slides and suppressions

---
+++ async-dolls/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("AsyncUsage", "AsyncFixer01:Unnecessary async/await usage", Justification = "<Pending>", Scope = "member", Target = "~M:AsyncDolls.AsyncStateWithDollsScript.NowItGetsALittleBitCrazy~System.Threading.Tasks.Task")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("AsyncUsage", "AsyncFixer01:Unnecessary async/await usage", Justification = "<Pending>", Scope = "member", Target = "~M:AsyncDolls.AsyncStateWithDollsScript.TheAsyncDollWithFloatingState~System.Threading.Tasks.Task")]
+
