0f1a4ca4eabaa2f8fe3ac2604752b1d4862c6809
Daniel Marbach <daniel.marbach@particular.net>
2016-06-09 17:35:07 +0200

    Showing OWIN and WebApi

---
+++ async-dolls/5-AsyncDolls/OwinMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using NUnit.Framework;
+using Owin;
+
+namespace AsyncDolls
+{
+    [TestFixture]
+    public class OwinMiddleware
+    {
+        [Test]
+        public void AppBuilder()
+        {
+            IAppBuilder appBuilder = null;
+
+            appBuilder.Use(async (ctx, next) =>
+            {
+                await next();
+            });
+        }
+    }
+
+    class FilterOutInvalidOperationException : IActionFilter
+    {
+        public bool AllowMultiple { get; }
+
+        public async Task<HttpResponseMessage> ExecuteActionFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
+        {
+            try
+            {
+                var response = await continuation();
+                return response;
+            }
+            catch (InvalidOperationException)
+            {
+            }
+
+            return new HttpResponseMessage();
+        }
+    }
+}
