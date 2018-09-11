54d579cf647292b3d7d6b72fa9675fdfd70ac5e9
danielmarbach <daniel.marbach@openplace.net>
2018-09-11 11:25:08 +0200

    More code prepared

---
+++ dishwasher/AsyncAutomaticDishwasherUnloading.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace dishwasher
+{
+    [TestFixture]
+    public class AsyncAutomaticDishwasherUnloading
+    {
+        [Test]
+        public async Task Unload()
+        {
+        }
+
+        static void Invoke(List<Action<Action>> actions, int currentIndex = 0)
+        {
+            if (currentIndex == actions.Count)
+            {
+                return;
+            }
+
+            var action = actions[currentIndex];
+            action(() => Invoke(actions, ++currentIndex));
+        }
+
+        public static void Son(Action next)
+        {
+            Console.WriteLine("Son");
+            next();
+        }
+
+        public static void Wife(Action next)
+        {
+            Console.WriteLine("Wife");
+            next();
+        }
+
+        public static void Husband(Action next)
+        {
+            Console.WriteLine("Husband");
+            next();
+        }
+
+        public static void Done(Action next)
+        {
+            Console.WriteLine("Dishwasher unloaded");
+        }
+    }
+}
---
+++ dishwasher/AsyncManualDishwasherUnloading.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static System.Console;
+
+namespace dishwasher
+{
+    [TestFixture]
+    public class AsyncManualDishwasherUnloading
+    {
+        [Test]
+        public async Task Unload()
+        {
+        }
+
+        public static void Son(Action next)
+        {
+            WriteLine("Son");
+            next();
+        }
+
+        public static void Wife(Action next)
+        {
+            WriteLine("Wife");
+            next();
+        }
+
+        public static void Husband(Action next)
+        {
+            WriteLine("Husband");
+            next();
+        }
+
+        public static void Done(Action next)
+        {
+            WriteLine("Dishwasher unloaded");
+            next();
+        }   
+    }
+}
